4ca8d3c09561acf9a8bb4c15b701ce2e4358563f
Jake Soenneker <jake@soenneker.com>
2023-03-19 17:26:51 -0500

    Initial push

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.MethodInfo.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.MethodInfo/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.methodinfo/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.methodinfo/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.MethodInfo.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.MethodInfo/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Extensions.MethodInfo
+### A collection of useful MethodInfo methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.MethodInfo
+```
---
+++ src/MethodInfoExtension.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Soenneker.Extensions.MethodInfo;
+
+public static class MethodInfoExtension
+{
+    /// <summary>
+    /// Decently heavy, use in logging situations etc..
+    /// </summary>
+    public static string GetSignature(this System.Reflection.MethodInfo? mi)
+    {
+        if (mi == null)
+            return "";
+
+        var result = "";
+
+        if (mi.IsPrivate)
+            result += "private";
+        else if (mi.IsPublic)
+            result += "public ";
+        if (mi.IsAbstract)
+            result += "abstract ";
+        if (mi.IsStatic)
+            result += "static ";
+        if (mi.IsVirtual)
+            result += "virtual ";
+
+        result += mi.ReturnType.Name + " ";
+
+        result += mi.Name + "(";
+
+        string[] param = mi.GetParameters()
+            .Select(p => $"{p.ParameterType.Name} {p.Name}")
+            .ToArray();
+
+        result += string.Join(", ", param);
+
+        result += ")";
+
+        return result;
+    }
+}
