7cd05fbfc34521571835de75e05bc55ff26d8227
Jake Soenneker <jake@soenneker.com>
2024-09-25 19:58:24 -0500

    Addt method

--- src/MethodInfoExtension.cs
+++ src/MethodInfoExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Text;
+using ParameterInfo = System.Reflection.ParameterInfo;
 
 namespace Soenneker.Extensions.MethodInfo;
 
@@ -8,41 +10,86 @@ namespace Soenneker.Extensions.MethodInfo;
 public static class MethodInfoExtension
 {
     /// <summary>
-    /// Decently heavy, use in logging situations etc.. TODO: StringBuilder?
+    /// Generates a string representation of the method's signature, including its access modifiers,
+    /// modifiers (abstract, static, virtual), return type, method name, and parameters.
     /// </summary>
+    /// <param name="methodInfo">
+    /// The <see cref="System.Reflection.MethodInfo"/> instance representing the method to generate the signature for.
+    /// </param>
+    /// <returns>
+    /// A string that represents the signature of the specified method. If <paramref name="methodInfo"/> is <c>null</c>,
+    /// an empty string is returned.
+    /// </returns>
     public static string GetSignature(this System.Reflection.MethodInfo? methodInfo)
     {
         if (methodInfo == null)
             return "";
 
-        var result = "";
+        var sb = new StringBuilder();
 
+        // Append access modifier
         if (methodInfo.IsPrivate)
-            result += "private";
+            sb.Append("private ");
         else if (methodInfo.IsPublic)
-            result += "public ";
+            sb.Append("public ");
 
+        // Append method modifiers
         if (methodInfo.IsAbstract)
-            result += "abstract ";
-
+            sb.Append("abstract ");
         if (methodInfo.IsStatic)
-            result += "static ";
+            sb.Append("static ");
+        if (methodInfo.IsVirtual && !methodInfo.IsAbstract)
+            sb.Append("virtual ");
 
-        if (methodInfo.IsVirtual)
-            result += "virtual ";
+        // Append return type and method name
+        sb.Append(methodInfo.ReturnType.Name);
+        sb.Append(' ');
+        sb.Append(methodInfo.Name);
+        sb.Append('(');
 
-        result += methodInfo.ReturnType.Name + " ";
+        // Append parameters
+        ParameterInfo[] parameters = methodInfo.GetParameters();
 
-        result += methodInfo.Name + "(";
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            ParameterInfo param = parameters[i];
+            sb.Append(param.ParameterType.Name);
+            sb.Append(' ');
+            sb.Append(param.Name);
 
-        string[] param = methodInfo.GetParameters()
-            .Select(p => $"{p.ParameterType.Name} {p.Name}")
-            .ToArray();
+            if (i < parameters.Length - 1)
+                sb.Append(", ");
+        }
 
-        result += string.Join(", ", param);
+        sb.Append(')');
 
-        result += ')';
+        return sb.ToString();
+    }
 
-        return result;
+    /// <summary>
+    /// Converts the <see cref="MethodInfo"/> instance's name to its original member name,
+    /// removing prefixes like "get_" or "set_" if the method represents a property accessor.
+    /// </summary>
+    /// <param name="methodInfo">The <see cref="MethodInfo"/> instance representing the method to extract the original name from.</param>
+    /// <returns>
+    /// A <see cref="string"/> containing the original member name, with any accessor prefixes removed.
+    /// </returns>
+    internal static string ToOriginalMemberName(this System.Reflection.MethodInfo methodInfo)
+    {
+        string methodName = methodInfo.Name;
+
+        // Check if it's a property getter or setter and remove "get_" or "set_"
+        if (methodInfo.IsSpecialName)
+        {
+            ReadOnlySpan<char> methodSpan = methodName.AsSpan();
+            int underscoreIndex = methodSpan.IndexOf('_');
+
+            if (underscoreIndex >= 0 && underscoreIndex < methodSpan.Length - 1)
+            {
+                methodName = methodSpan.Slice(underscoreIndex + 1).ToString();
+            }
+        }
+
+        return methodName;
     }
 }
